0cca09024f8eddda045c3a9f492037e6b9bf300d
hanna_pleshko <anna-pleshko2000@mail.ru>
2021-04-06 12:52:15 +0300

    Added classes for viewing all published movies and viewing an individual movie by slug. Also html, css for these paths

--- ListMovies/django_movies/movies/urls.py
+++ ListMovies/django_movies/movies/urls.py
@@ -2,6 +2,24 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', MoviesView.as_view())
+    path('', MoviesView.as_view()),
+    path("<slug:slug>/", MovieDetailView.as_view(), name="movie_detail"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- ListMovies/django_movies/movies/views.py
+++ ListMovies/django_movies/movies/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import *
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movie_list.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
+    template_name = 'movie_detail.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
