9478c62b50cad3569253d6b4576375a6d4c480c9
hanna_pleshko <hannapleshko2000@mail.ru>
2021-04-12 17:26:51 +0300

    Added work with rating

--- ListMovies/django_movies/movies/urls.py
+++ ListMovies/django_movies/movies/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', MoviesView.as_view()),
+    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("filter/", FilterMoviesView.as_view(), name='filter'),
     path("<slug:slug>/", MovieDetailView.as_view(), name="movie_detail"),
     path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
