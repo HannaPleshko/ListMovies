2951135dfc7bbf4fa26422ba9a4812237ee15e11
hanna_pleshko <hannapleshko2000@mail.ru>
2021-04-13 15:16:31 +0300

    Added search

--- ListMovies/django_movies/movies/urls.py
+++ ListMovies/django_movies/movies/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', MoviesView.as_view()),
-    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("filter/", FilterMoviesView.as_view(), name='filter'),
+    path("search/", Search.as_view(), name='search'),
+    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("<slug:slug>/", MovieDetailView.as_view(), name="movie_detail"),
     path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
     path("actor/<str:slug>/", ActorView.as_view(), name="actor_detail"),
